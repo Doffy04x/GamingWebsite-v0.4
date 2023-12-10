acb801c2c9ea3641975bd3913861d58906b2f7ff
Doffy04x <149186919+Doffy04x@users.noreply.github.com>
2023-12-10 21:14:34 +0100

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyAbiIBs16w1xT-7jnepaN2DvWu2t_6E7yg",
+          authDomain: "tic-tac-toe-88718.firebaseapp.com",
+          projectId: "tic-tac-toe-88718",
+          storageBucket: "tic-tac-toe-88718.appspot.com",
+          messagingSenderId: "340009264879",
+          appId: "1:340009264879:web:c90a8685a907ceb9f9dd1a",
+        }
+      }
+    }
+  ],
+}
